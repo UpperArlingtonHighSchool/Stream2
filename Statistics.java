4e2de29432b4d870a9ea28b64e9ae9ab59e3dd87
Rhea Pathak <105311775+rheapathak@users.noreply.github.com>
2022-05-16 13:52:25 -0400

    Update Statistics.java
    one var stats variables instantiated, set up text output for formatting

--- Statistics.java
+++ Statistics.java
@@ -1,7 +1,49 @@
 import java.util.ArrayList;
 
+//standard deviation
+//mean
+//median
+//quartiles
+//r squared
+
 public class Statistics 
 {
+	private int pmean;
+	private int psd;
+	private int pmedian;
+	private int pmin;
+	private int pq1;
+	private int pq3;
+	private int pmax;
+	private int prsqrd;
+	
+	private int tmean;
+	private int tsd;
+	private int tmedian;
+	private int tmin;
+	private int tq1;
+	private int tq3;
+	private int tmax;
+	private int trsqrd;
+	
+	
+/*	
+	private int twomean;
+	private int twosd;
+	private int twomedian;
+	private int twomin;
+	private int twoq1;
+	private int twoq3;
+	private int twomax;
+	private int twosqrd;
+
+	private String Twovarstatoutput;
+	
+*/	
+	private String PhosOnevarstatoutput;
+	private String TurbOnevarstatoutput;
+	
+	
 	ArrayList<Measurement> meas;
 	
 	public Statistics(ArrayList<Measurement> m)
@@ -11,12 +53,24 @@ public class Statistics
 	
 	public String oneVarPhos()
 	{
-		 
-		return null;
+		PhosOnevarstatoutput = ("Mean of Phosphate data: " + pmean);
+		PhosOnevarstatoutput += ("\nStandard Deviation of phosphate data: " + psd);
+		PhosOnevarstatoutput += ("\nMedian of phosphate data: " + pmean);
+		PhosOnevarstatoutput += ("\n5 number summary of phosphate data: " + pmin + ", " + pq1 + ", " + pmedian + ", " + pq3 + ", "+ pmax);
+		PhosOnevarstatoutput += ("\nCoefficient of determination (r squared) for phosphate data: " + prsqrd);
+		return PhosOnevarstatoutput;
 	}
 	
 	public String oneVarTurb()
 	{
+		TurbOnevarstatoutput = ("Mean of turbidity data: " + tmean);
+		TurbOnevarstatoutput += ("\nStandard Deviation of turbidity data: " + tsd);
+		TurbOnevarstatoutput += ("\nMedian of turbidity data: " + tmean);
+		TurbOnevarstatoutput += ("\n5 number summary of turbidity data: " + tmin + ", " + tq1 + ", " + tmedian + ", " + tq3 + ", "+ tmax);
+		TurbOnevarstatoutput += ("\nCoefficient of determination (r squared) for turbidity data: " + trsqrd);
+		return TurbOnevarstatoutput;
+	}
+	public String twoVar() {
 		
 		return null;
 	}
